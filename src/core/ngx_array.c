9f9424998388e391952c612ab48c3595bb55c066
ifengbaobao <jsfjb@163.com>
2022-06-22 23:41:16 +0800

    对数组的创建销毁和存放数据。数组的大小不够添加数据时可以动态扩大。

--- src/core/ngx_array.c
+++ src/core/ngx_array.c
@@ -1,13 +1,14 @@
 
 /*
  * Copyright (C) Igor Sysoev
+  对数组的创建销毁和存放数据。
  */
 
 
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-
+//创建一个数组。
 ngx_array_t *ngx_create_array(ngx_pool_t *p, ngx_uint_t n, size_t size)
 {
     ngx_array_t *a;
@@ -24,7 +25,7 @@ ngx_array_t *ngx_create_array(ngx_pool_t *p, ngx_uint_t n, size_t size)
     return a;
 }
 
-
+//销毁数组，如果申请的空间，正好在内存池最后的位置，直接从内存池中回收。
 void ngx_destroy_array(ngx_array_t *a)
 {
     ngx_pool_t  *p;
@@ -40,7 +41,7 @@ void ngx_destroy_array(ngx_array_t *a)
     }
 }
 
-
+//返回未使用的空间的指针地址。数组的空间不够会自动扩充。
 void *ngx_push_array(ngx_array_t *a)
 {
     void        *elt, *new;
@@ -51,6 +52,7 @@ void *ngx_push_array(ngx_array_t *a)
         p = a->pool;
 
         /* array allocation is the last in the pool */
+		//如果正好在内存池末尾，内存池还有空间，直接扩容。
         if ((char *) a->elts + a->size * a->nelts == p->last
             && (unsigned) (p->end - p->last) >= a->size)
         {
@@ -58,7 +60,7 @@ void *ngx_push_array(ngx_array_t *a)
             a->nalloc++;
 
         /* allocate new array */
-        } else {
+        } else {//重新申请空间。
             ngx_test_null(new, ngx_palloc(p, 2 * a->nalloc * a->size), NULL);
 
             ngx_memcpy(new, a->elts, a->nalloc * a->size);
--- src/core/ngx_array.h
+++ src/core/ngx_array.h
@@ -1,6 +1,7 @@
 
 /*
  * Copyright (C) Igor Sysoev
+ 对数组的创建销毁和存放数据。
  */
 
 
@@ -11,21 +12,23 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-
+//存放数组的结构体。
 struct ngx_array_s {
-    void        *elts;
-    ngx_uint_t   nelts;
-    size_t       size;
-    ngx_uint_t   nalloc;
-    ngx_pool_t  *pool;
+    void        *elts;//存放数据的指针。
+    ngx_uint_t   nelts;//已经使用的数组的个数。
+    size_t       size;//数组的每个值多大。
+    ngx_uint_t   nalloc;//数组的总大小，总的能放多少个。
+    ngx_pool_t  *pool;//内存池。
 };
 
-
+//创建一个数组。
 ngx_array_t *ngx_create_array(ngx_pool_t *p, ngx_uint_t n, size_t size);
+//销毁数组，如果申请的空间，正好在内存池最后的位置，直接从内存池中回收。
 void ngx_destroy_array(ngx_array_t *a);
+//返回未使用的空间的指针地址。数组的空间不够会自动扩充。
 void *ngx_push_array(ngx_array_t *a);
 
-
+//数组初始化。
 ngx_inline static ngx_int_t ngx_array_init(ngx_array_t *array, ngx_pool_t *pool,
                                            ngx_uint_t n, size_t size)
 {
@@ -42,7 +45,7 @@ ngx_inline static ngx_int_t ngx_array_init(ngx_array_t *array, ngx_pool_t *pool,
 }
 
 
-
+//数组初始化。
 #define ngx_init_array(a, p, n, s, rc)                                       \
     ngx_test_null(a.elts, ngx_palloc(p, n * s), rc);                         \
     a.nelts = 0; a.size = s; a.nalloc = n; a.pool = p;
